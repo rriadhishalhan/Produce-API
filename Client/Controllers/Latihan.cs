b3958551a00ba0708aa93d8efbcc53e5cc2d055b
rriadhishalhan <rezkyriadhishalhan@gmail.com>
2022-03-17 11:29:26 +0700

    base

--- Client/Controllers/Latihan.cs
+++ Client/Controllers/Latihan.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Client.Controllers
 {
-    public class LatihanController : Controller
+    public class Latihan : Controller
     {
         public IActionResult Index()
         {
